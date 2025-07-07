3082670e7025ab55536f6ad08ae3494cf06fec1a
Theo Justin <theojustinx@gmail.com>
2025-07-07 12:36:05 +0700

    fix : fixed some minor css in header

--- src/sunshinev2_frontend/src/components/Header.jsx
+++ src/sunshinev2_frontend/src/components/Header.jsx
@@ -75,7 +75,7 @@ function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <motion.div
-            className="flex items-center space-x-2"
+            className="flex items-center space-x-2 cursor-pointer"
             onClick={() => nav('/')}
             whileHover={{ scale: 1.05 }}
             transition={{ type: 'spring', stiffness: 400, damping: 10 }}
